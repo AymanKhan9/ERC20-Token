1cb7c4a6462a5faf1e3ecb7ae5439fc4cf9aa85e
Ayman Khan <ayman.cs22@bmsce.ac.in>
2025-10-27 15:01:34 +0530

    Added env example

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ require("dotenv").config()
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 
 module.exports = {
   solidity: "0.8.20",
@@ -17,8 +17,5 @@ module.exports = {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [PRIVATE_KEY],
     },
-  },
-    etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
+  }
 };
